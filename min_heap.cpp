027dcc8fd759028a51684552d38be95062efae3e
Vladimir <vlasmur@localhost.localdomain>
2017-03-22 02:48:46 +0300

    added insert method

--- min_heap.cpp
+++ min_heap.cpp
@@ -2,27 +2,19 @@
 #include <vector>
 #include <utility>
 
+template <typename T>
 class MinHeap {
  private:
-    std::vector<int> heap;
+    std::vector<T> heap;
 
  public:
-    MinHeap(const std::vector<int>& v)
+    MinHeap(const std::vector<T>& v)
     : heap(v) {
         make_heap();
         print();
     }
 
-    void heapify_up(size_t i) {
-        if (i > 0) {
-            size_t j = (i - 1) / 2;
-            if (heap[j] > heap[i])
-                std::swap(heap[i], heap[j]);
-            heapify_up(j);
-        }
-    }
-
-    void max_heapify(int i) {
+    void min_heapify(int i) {
         int min = i, l = 2 * i + 1, r = 2 * i + 2;
         if (l < heap.size() && heap[l] < heap[i])
             min = l;
@@ -30,13 +22,27 @@ class MinHeap {
             min = r;
         if (min != i) {
             std::swap(heap[i], heap[min]);
-            max_heapify(min);
+            min_heapify(min);
         }
     }
 
     void make_heap() {
         for (int i = heap.size() / 2; i >= 0; --i)
-            max_heapify(i);
+            min_heapify(i);
+    }
+
+    void heapify_up(int i) {
+        if (i > 0) {
+            int j = (i - 1) / 2;
+            if (heap[j] > heap[i])
+                std::swap(heap[i], heap[j]);
+            heapify_up(j);
+        }
+    }
+
+    void insert(T x) {
+        heap.push_back(x);
+        heapify_up(heap.size() - 1);
     }
 
     void print() {
@@ -47,5 +53,5 @@ class MinHeap {
 
 int main() {
     std::vector<int> v {17, 23, 8, 19, 5, 11, 3};
-    MinHeap mh = MinHeap(v);
+    MinHeap<int> mh = MinHeap<int>(v);
 }
